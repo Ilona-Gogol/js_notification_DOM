09ef5cc5a6bf9befb8a8cdb50faf287ab0589002
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-14 20:45:32 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const divElement = document.createElement("div");
+    const heading = document.createElement("h2");
+    const paragraph = document.createElement("p");
+    divElement.classList.add("notification", type);
+    divElement.style.top = `${posTop}px`;
+    divElement.style.right = `${posRight}px`;
+    heading.classList.add("title");
+    heading.textContent = title;
+    paragraph.textContent = description;
+    divElement.appendChild(heading);
+    divElement.appendChild(paragraph);
+    document.body.appendChild(divElement);
+    setTimeout(()=>{
+        divElement.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(160, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
